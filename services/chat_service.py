ef0c20e77841d02626aaff3b85a1f22fa3aae396
tarun3250 <tarun03250@gmail.com>
2026-06-15 19:21:20 +0530

    implemented chat history cleanup service

--- services/chat_service.py
+++ services/chat_service.py
@@ -18,10 +18,42 @@ def save_chat(question, answer):
     cursor.close()
     connection.close()
 
-if __name__ == "__main__":
-    save_chat(
-        "What is FastAPI?",
-        "FastAPI is a Python web framework."
-    )
 
-    print("Chat saved successfully")
+def get_chat_history():
+    connection = get_connection()
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        SELECT id, question, answer, created_at
+        FROM chats
+        ORDER BY created_at ASC
+    """)
+
+    chats = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return chats
+# print(get_chat_history())
+# print("all data printed")
+
+def clear_chat_history():
+
+    connection = get_connection()
+
+    cursor = connection.cursor()
+
+    cursor.execute(
+        """
+        DELETE FROM chats
+        """
+    )
+    connection.commit()
+    cursor.close()
+    connection.close()
+
+# if __name__ == "__main__":
+#     clear_chat_history()
+#     print("chat history cleared")
