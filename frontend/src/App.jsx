260673c48949cbb9d81ca8ead2a80db8b0668179
tarun3250 <tarun03250@gmail.com>
2026-06-16 14:32:28 +0530

    react state management and question input feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react";
+
 function App() {
+
+  const [question, setQuestion] = useState("");
+  const [answer, setAnswer] = useState("");
+
   return (
     <div>
       <h1>AI Knowledge Assistant</h1>
+
+      <input
+        type="text"
+        placeholder="Ask a question..."
+        value={question}
+        onChange={(e) => setQuestion(e.target.value)}
+      />
+      
+      <button>
+        Ask
+      </button>
+
+      <h3>Response:</h3>
+      <p>{answer}</p>
+
     </div>
   );
 }
